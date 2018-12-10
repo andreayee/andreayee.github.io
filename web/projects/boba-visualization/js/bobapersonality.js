b77ebb39baae15d65435446985af571fb9978bf5
andrea <andrea.yee@nyu.edu>
2018-12-10 19:27:06 +0800

    finished boba personality quiz

---
+++ web/projects/boba-visualization/js/bobapersonality.js
@@ -0,0 +1,164 @@
+function resetTest () {
+
+	var x = document.getElementsByClassName("personality-result");
+
+
+	var i;
+	for (i = 0; i < x.length; i++) {
+		x[i].style.display = "none";
+	}
+
+	window.location.href = "#boba-personality";
+
+}
+
+
+
+
+
+
+function showOriginalMTResult() {
+
+	document.getElementById("original-mt-result").style.display = 'block';
+	window.location.href = "#original-mt-result";
+	document.getElementById("reset-test").style.display = 'block';
+
+
+}
+
+function showTaroMTResult() {
+	document.getElementById("taro-mt-result").style.display = 'block';
+	window.location.href = "#taro-mt-result";
+	document.getElementById("reset-test").style.display = 'block';
+
+
+}
+
+function showThaiMTResult() {
+	document.getElementById("thai-mt-result").style.display = 'block';
+	window.location.href = "#thai-mt-result";
+	document.getElementById("reset-test").style.display = 'block';
+
+
+}
+
+function showJasmineMTResult() {
+	document.getElementById("jasmine-mt-result").style.display = 'block';
+	window.location.href = "#jasmine-mt-result";
+	document.getElementById("reset-test").style.display = 'block';
+
+
+}
+
+function showLavenderMTResult() {
+	document.getElementById("lavender-mt-result").style.display = 'block';
+	window.location.href = "#lavender-mt-result";
+	document.getElementById("reset-test").style.display = 'block';
+
+
+}
+
+function showHazelnutMTResult() {
+	document.getElementById("hazelnut-mt-result").style.display = 'block';
+	window.location.href = "#hazelnut-mt-result";
+	document.getElementById("reset-test").style.display = 'block';
+
+}
+
+function showTigerMTResult() {
+	document.getElementById("tiger-mt-result").style.display = 'block';
+	window.location.href = "#tiger-mt-result";
+	document.getElementById("reset-test").style.display = 'block';
+
+}
+
+function showMatchaMTResult() {
+	document.getElementById("matcha-mt-result").style.display = 'block';
+	window.location.href = "#matcha-mt-result";
+	document.getElementById("reset-test").style.display = 'block';
+
+}
+
+function showHoneydewMTResult() {
+	document.getElementById("honeydew-mt-result").style.display = 'block';
+	window.location.href = "#honeydew-mt-result";
+	document.getElementById("reset-test").style.display = 'block';
+
+}
+
+function showCoffeeMTResult() {
+	document.getElementById("coffee-mt-result").style.display = 'block';
+	window.location.href = "#coffee-mt-result";
+	document.getElementById("reset-test").style.display = 'block';
+
+}
+
+function showOolongMTResult() {
+	document.getElementById("oolong-mt-result").style.display = 'block';
+	window.location.href = "#oolong-mt-result";
+	document.getElementById("reset-test").style.display = 'block';
+
+}
+
+function showWintermelonMTResult() {
+	document.getElementById("wintermelon-mt-result").style.display = 'block';
+	window.location.href = "#wintermelon-mt-result";
+	document.getElementById("reset-test").style.display = 'block';
+
+}
+
+function showHokkaidoMTResult() {
+	document.getElementById("hokkaido-mt-result").style.display = 'block';
+	window.location.href = "#hokkaido-mt-result";
+	document.getElementById("reset-test").style.display = 'block';
+
+}
+
+function showOkinawaMTResult() {
+	document.getElementById("okinawa-mt-result").style.display = 'block';
+	window.location.href = "#okinawa-mt-result";
+	document.getElementById("reset-test").style.display = 'block';
+
+}
+
+function showRoseMTResult() {
+	document.getElementById("rose-mt-result").style.display = 'block';
+	window.location.href = "#rose-mt-result";
+	document.getElementById("reset-test").style.display = 'block';
+
+}
+
+function showFruityTResult() {
+	document.getElementById("fruity-t-result").style.display = 'block';
+	window.location.href = "#fruity-t-result";
+	document.getElementById("reset-test").style.display = 'block';
+
+}
+
+function showLycheeSlushResult() {
+	document.getElementById("lychee-slush-result").style.display = 'block';
+	window.location.href = "#lychee-slush-result";
+	document.getElementById("reset-test").style.display = 'block';
+
+}
+
+function showStrawberrySlushResult() {
+	document.getElementById("strawberry-slush-result").style.display = 'block';
+	window.location.href = "#strawberry-slush-result";
+	document.getElementById("reset-test").style.display = 'block';
+
+}
+
+function showMangoSlushResult() {
+	document.getElementById("mango-slush-result").style.display = 'block';
+	window.location.href = "#mango-slush-result";
+	document.getElementById("reset-test").style.display = 'block';
+
+}
+
+function showPassionfruitSlushResult() {
+	document.getElementById("passionfruit-slush-result").style.display = 'block';
+	window.location.href = "#passionfruit-slush-result";
+	document.getElementById("reset-test").style.display = 'block';
+
+}
